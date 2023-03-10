9ee71589c3fba148e9f0e7d07faf95502abb4b5d
Brandon Begle <brandon@begleweyer.com>
2023-03-10 14:02:44 -0500

    Save to file, read from file, shuffle

--- deck.go
+++ deck.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
+	"os"
 	"strings"
+	"time"
 )
 
 // Create a new type of 'deck'
@@ -31,10 +34,37 @@ func newDeck() deck {
 	return cards
 }
 
+func newDeckFromFile(filename string) deck {
+	bs, err := os.ReadFile(filename)
+	if err != nil {
+		fmt.Println("Error: ", err)
+		os.Exit(1)
+	}
+
+	newDeckString := string(bs)
+
+	newDeck := strings.Split(newDeckString, ",")
+
+	return newDeck
+}
+
 func deal(d deck, handSize int) (deck, deck) {
 	return d[:handSize], d[handSize:]
 }
 
 func (d deck) toString() string {
 	return strings.Join([]string(d), ",")
+}
+
+func (d deck) saveToFile(filename string) error {
+	return os.WriteFile(filename, []byte(d.toString()), 0666)
+}
+
+func (d deck) shuffle() {
+	source := rand.NewSource(time.Now().UnixNano())
+	r := rand.New(source)
+	for i := range d {
+		newPosition := r.Intn(len(d) - 1)
+		d[i], d[newPosition] = d[newPosition], d[i]
+	}
 }
--- main.go
+++ main.go
@@ -1,13 +1,15 @@
 package main
 
-import "fmt"
-
 func main() {
 	cards := newDeck()
+	cards.shuffle()
+	cards.print()
 	//
 	//hand, remainingCards := deal(cards, 5)
 	//
 	//hand.print()
 	//remainingCards.print()
-	fmt.Println(cards.toString())
+	//fmt.Println(cards.toString())
+	//cards.saveToFile("mycards.txt")
+	//fmt.Println(cards)
 }
